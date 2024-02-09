d886a19ae4fc78026e811644199eff5cf34cf938
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-02-09 13:39:30 +0500

    Create 06.cpp

---
+++ LABS/lab02/06.cpp
@@ -0,0 +1,30 @@
+/*You are asked to write a C++ program that calculates the Least Common Multiple (LCM)
+and Greatest Common Divisor (GCD) of two numbers. Implement two separate recursive
+functions, calculateLCM and calculateGCD, each handling the corresponding task.*/
+#include <iostream>
+using namespace std;
+int CalculateGCD(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return CalculateGCD(b, a % b);
+    }
+}
+int CalculateLCM(int a, int b)
+{
+    return (a * b) / CalculateGCD(a, b);
+}
+int main()
+{
+    int m, n;
+    cout << "Enter two numbers" << endl;
+    cin >> m >> n;
+    int x = CalculateGCD(m, n);
+    int y = CalculateLCM(m, n);
+    cout << "Greatest Common Divisor is " << x << endl;
+    cout << "Least Common Multiple is " << y << endl;
+}
