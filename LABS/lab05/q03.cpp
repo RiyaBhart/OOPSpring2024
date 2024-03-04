2c4037bed6ee64ecffc55e505285bc64f2625b0f
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2024-03-04 12:39:48 +0500

    Create q03.cpp

---
+++ LABS/lab05/q03.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cctype> // For isalpha() function; used to check whether the character is an alphabet or anything else
+using namespace std;
+
+class ValidateString {
+    string s;
+
+public:
+    ValidateString(string s) {
+        this->s = s;
+    }
+
+    
+    bool isValid() const {
+        for (char ch : s) {
+            if (!isalpha(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    
+    ValidateString str1("HelloWorld");
+    ValidateString str2("11234Name");
+    ValidateString str3("*()%^@#$");
+
+   
+    cout << "String 1 is " << (str1.isValid() ? "valid" : "invalid") << endl;
+    cout << "String 2 is " << (str2.isValid() ? "valid" : "invalid") << endl;
+    cout << "String 3 is " << (str3.isValid() ? "valid" : "invalid") << endl;
+
+    return 0;
+}
